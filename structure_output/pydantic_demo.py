985fbfb9202c09dc1a53b1082fd2f457c4b5d613
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-08 16:15:18 +0530

    pydantic feat added in this code

---
+++ structure_output/pydantic_demo.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import Annotated , Optional
+
+
+class Person(BaseModel):
+    name:str= Field(description="name of the person")
+    age:Optional[int] = Field(default= None, description="age of the person (1-100)")
+    city:str = Field(description="city of the person (state)")
+
+new_person = Person(name="Ali", age=23, city="alwer")
+
+print(new_person.model_dump())
+
+new_person1 = Person(name="Ali", city="alwer")
+
+print(new_person1.model_dump())
