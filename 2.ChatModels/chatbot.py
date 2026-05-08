8e8103f6f87d726789ffe63f8ab1cf9477285b90
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-08 13:50:16 +0530

    fix:chat history and storage problem

--- 2.ChatModels/chatbot.py
+++ 2.ChatModels/chatbot.py
@@ -4,9 +4,15 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
+chat_history = []
+
 while True:
     user_input = input("you : ")
+    chat_history.append(user_input)
     if user_input == "exit":
         break
-    response = model.invoke([user_input])
+    response = model.invoke(chat_history)
+    chat_history.append(response.content)
     print("bot : ", response.content)
+
+print(chat_history)
