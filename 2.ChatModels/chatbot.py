dae23d3d24f6838fdda8508bb776a96f3bca5130
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-08 14:04:50 +0530

    feat: implement interactive chatbot interface using Google Gemini and LangChain

--- 2.ChatModels/chatbot.py
+++ 2.ChatModels/chatbot.py
@@ -1,18 +1,21 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage,SystemMessage,AIMessage
 from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful assistant.")
+]
 
 while True:
     user_input = input("you : ")
-    chat_history.append(user_input)
     if user_input == "exit":
         break
+    chat_history.append(HumanMessage(content=user_input))
     response = model.invoke(chat_history)
-    chat_history.append(response.content)
     print("bot : ", response.content)
+    chat_history.append(AIMessage(content=response.content))
 
 print(chat_history)
