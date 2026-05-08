f91b76d6d6803570522cc5712a8282abe2845fe8
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-08 16:07:32 +0530

    feat:annotated used in this code

--- structure_output/with_structured_output_typeddict.py
+++ structure_output/with_structured_output_typeddict.py
@@ -1,6 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
@@ -8,9 +8,9 @@ model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 class Review (TypedDict):
     """schema for the review model"""
-    summary : str
-    sentiment:str
-    rating: int
+    summary : Annotated[str, "summary of the review (2-3 lines)"]
+    sentiment:Annotated[str, "sentiment of the review (positive, negative, neutral)"]
+    rating: Annotated[int, "rating of the review (1-5)"]
 
 structure_model = model.with_structured_output(Review)
 
